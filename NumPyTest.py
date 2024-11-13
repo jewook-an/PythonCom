3f6df3a303b95e52b68c4e9b44a562359f866a75
jewook-an <83323568+jewook-an@users.noreply.github.com>
2024-11-13 19:18:12 +0900

    NumPyTest.py 업데이트

--- NumPyTest.py
+++ NumPyTest.py
@@ -1,1 +1,12 @@
+# 컴포넌트 초기화
+numpy_utils = CommonNumpy()
 
+# 배열 생성
+array1 = numpy_utils.create_array([1, 2, 3, 4, 5])
+array2 = numpy_utils.create_array([6, 7, 8, 9, 10])
+
+# 행렬 연산
+result = numpy_utils.matrix_operations(array1, array2, 'add')
+
+# 통계 분석
+stats = numpy_utils.statistical_analysis(array1)
